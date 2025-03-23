da522d4e38bced39d1f15d19b0bbf2ee264a29d6
vika <37137412+hi-im-vika@users.noreply.github.com>
2025-03-23 16:42:32 -0700

    Disable gamepad controls for ImGui for testing

--- src/CMain.cpp
+++ src/CMain.cpp
@@ -72,7 +72,7 @@ CMain::CMain() {
     ImGuiIO &io = ImGui::GetIO();
     (void) io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+//    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
     io.ConfigDockingTransparentPayload = true;
     ImGui::StyleColorsDark();
